0030afe1660f116e8e22c80e6f3e96adff1ead01
Yohann Coppel <yc@leppoc.net>
2015-07-31 23:40:16 -0700

    Adding code

---
+++ README.md
@@ -0,0 +1,127 @@
+
+Plottyjs plots functions in your HTML with d3js.
+
+It is not meant to become interactive or fully featured, it was written to
+embed plots in documentation, blogs, etc.
+
+# Documentation
+
+## Using javascript
+
+Using javascript, a svg element is appended right after the `script` tag.
+
+Single parametric plot:
+
+```
+<script>
+  var plot = plottyjs.newPlot(300, 300, {
+    xbounds: [-1.2, 1.2],
+    ybounds: [-1.2, 1.2],
+    t: [0, 2*Math.PI],
+    samples: 300
+  });
+  plot.parametric('sin(2*t)', 'sin(3*t)');
+</script>
+```
+
+Multiple plots:
+
+```
+<script>
+  var plot = plottyjs.newPlot(400, 200, {
+    t: [-5, 5],
+    ybounds: [-2.5, 2.5],
+    samples: 300
+  });
+  plot.cartesian('sin(x)').style('stroke', 'lightblue');
+  plot.cartesian('4 / (1 + exp(-x))-2');
+  plot.cartesian('sqrt(x)').style('stroke', 'lightgreen');
+  plot.cartesian('tan(x)+0.5').style('stroke', 'lightcoral');
+  plot.cartesian('1/x').style('stroke', 'lightslategray');
+  plot.cartesian('1/(x-pi)+0.5').style('stroke', 'lightslategray');
+  plot.cartesian('x^2').style('stroke', 'lightpink');
+</script>
+```
+
+Parameters to the newPlot function are the width and height of the svg element
+to be created, followed by options.
+
+Options are:
+
+- `t` defines the range of sampling, defaults to [-1, 1],
+- `samples` defines how many samples ar taken in the range `t` and defaults to 100,
+- `xbounds` for boundaries of the `x` axis, defaults to automatically finding
+  the bounds. If they are automatically determined and multiple equations are
+  plotted, `xbounds` is determined from the first one plotted only.
+- `ybounds` is the same for the `y` axis.
+- `margin` adds a margin to the svg element and defaults to 2 (it looks nicer
+  if this matches the stroke-width used in the plot.)
+
+## Using HTML tags
+
+Using HTML tags, an svg element is added as a child of the element.
+
+```
+<span class="plot" id="asym" data-y="1/(x-pi)"
+      data-from="-3" data-to="6"
+      data-bottom="-15" data-top="15"
+      data-width="300" data-height="100"></span>
+```
+Once the document is loaded, run the following function once, will process
+every plot on the page with a `plot` class for example:
+
+```
+plottyjs.makePlots(document.querySelectorAll('.plot'));
+```
+
+Data attributes correspond to Options above:
+
+- `from` and `to` are set to `t`
+- `left`, `right` map to `xbouds`, while `bottom` and `top` map to `ybounds`
+- `samples` and `margin` are obvious
+
+Then, two extra data attributes are available: `width` and `height`. They
+define the svg size and are required.
+
+Finally data attributes `x`, `y` and `r` define the function to render, and at
+least some combination of them must be specified:
+
+- if both `x` and `y` are specified, a parametric equations is rendered with
+  `x=f(t)` and `y=f(t)` (i.e. use `t` as variable name in the function),
+- if `r` is defined then a polar equation is rendered with `r=f(o)`,
+- Otherwise `x` must define a cartesian equation as `y=f(x)`
+
+Another example:
+
+```
+<span class="plot" id="polar2" data-r="sin(o)+(sin(5*o/2))^3"
+      data-from="0" data-to="12.5663706144"
+      data-samples="300"
+      data-width="200" data-height="200"></span>
+```
+
+By leaving out `left`, `right`, `top` and `bottom` the bounds are automatically
+computed and the plot fills the whole svg. In that situation, care must be
+taken with the selection of `width` and  `height`.
+
+
+## Writing functions
+
+When specifying functions as parameters to the `cartesian`, `parametric` or
+`polar` functions, you can use either a string or a javascript function.
+
+When specifying functions in the `data-x`, `data-y` or `data-r` html
+attributes, only strings can be used.
+
+Strings are parsed using Math.js.
+
+# Dependencies
+
+- [Math.js](http://mathjs.org)
+- [D3.js](http://d3js.org/)
+
+For CDN hosted versions:
+```
+<script src="https://cdnjs.cloudflare.com/ajax/libs/mathjs/2.0.1/math.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js" charset="utf-8"></script>
+```
---
+++ plottyjs.js
@@ -0,0 +1,254 @@
+var plottyjs = {};
+(function(){
+  this.makePlots = function(els) {
+    for (var i = 0; i < els.length; ++i) {
+      var el = els[i];
+
+      var svg = d3.select(el).append('svg');
+      svg.attr("width", el.dataset.width).attr("height", el.dataset.height);
+
+      var chart = new this.Plot(svg, {
+        t: el.dataset.from
+            ? [Number.parseFloat(el.dataset.from), Number.parseFloat(el.dataset.to)]
+            : null,
+        xbounds: el.dataset.left
+            ? [Number.parseFloat(el.dataset.left), Number.parseFloat(el.dataset.right)]
+            : null,
+        ybounds: el.dataset.bottom
+            ? [Number.parseFloat(el.dataset.bottom), Number.parseFloat(el.dataset.top)]
+            : null,
+        samples: el.dataset.samples
+            ? Number.parseFloat(el.dataset.samples)
+            : null,
+        margin: el.dataset.margin
+            ? Number.parseFloat(el.dataset.margin)
+            : null,
+      });
+
+      if (el.dataset.y && el.dataset.x) {
+        chart.parametric(el.dataset.x, el.dataset.y);
+      } else if (el.dataset.r) {
+        chart.polar(el.dataset.r);
+      } else {
+        chart.cartesian(el.dataset.y);
+      }
+    }
+  };
+
+  this.newPlot = function(width, height, options) {
+    var a = d3.selectAll("script");
+    var script = a[0][a[0].length - 1];
+    var svg = d3.select(script.parentNode).append("svg");
+    svg.attr("width", width).attr("height", height);
+    return new this.Plot(svg, options);
+  };
+
+  this.Plot = function(root, options) {
+    this.svg = root;
+    this.t = options.t || [-1, 1];
+    this.samples = options.samples || 100;
+    this.xbounds = options.xbounds;
+    this.ybounds = options.ybounds;
+    this.margin = options.margin || 2;
+    console.log(this.t);
+
+    this.width = root.attr("width");
+    this.height = root.attr("height");
+
+    this.xScale = undefined;
+    this.yScale = undefined;
+  };
+
+  this.Plot.prototype.cartesian = function (y, t) {
+      if (typeof y == 'string') {
+          var f = math.compile(y);
+          y = function (x) {
+              return f.eval({
+                  x: x
+              });
+          };
+      }
+      var line = this.plot(identity, y);
+      return line;
+  };
+
+  this.Plot.prototype.polar = function (r) {
+      if (typeof r == 'string') {
+        var f = math.compile(r);
+        r = function(o) {
+          return f.eval({
+              o: o
+          });
+        };
+      }
+      var line = this.plot(function (o) {
+          return r(o) * Math.cos(o);
+      }, function (o) {
+          return r(o) * Math.sin(o);
+      });
+      return line;
+  };
+
+  this.Plot.prototype.parametric = function (x, y) {
+      if (typeof x == 'string') {
+        var fx = math.compile(x);
+        x = function (t) {
+          return fx.eval({
+              t: t
+          });
+        };
+      }
+      if (typeof y == 'string') {
+        var fy = math.compile(y);
+        y = function (t) {
+          return fy.eval({
+              t: t
+          });
+        };
+      }
+      var line = this.plot(x, y);
+      return line;
+  };
+
+  this.Plot.prototype.plot = function (fx, fy) {
+    var dt = (this.t[1] - this.t[0]) / this.samples;
+    var ts = d3.range(this.t[0], this.t[1] + dt, dt);
+
+    // Array of Arrays of points, for each piece of the function.
+    // Functions with no vertical asymptotes have a single 'piece'.
+    var pieces = [];
+    // Array of points currently being added.
+    var pts = [];
+
+    var lastPt = null;
+    var lastT = null;
+    for (var i = 0; i < ts.length; i++) {
+        t1 = ts[i];
+        var x1 = fx(t1);
+        var y1 = fy(t1);
+        var pt = [x1, y1];
+
+        if (Number.isNaN(x1) || !Number.isFinite(x1) || Number.isNaN(y1) || !Number.isFinite(y1)) {
+            if (pts.length > 0) pieces.push(pts);
+            pts = [];
+            lastPt = null;
+        } else if (checkAsymptote(fx, fy, lastT, lastPt, t1, pt, 0)) {
+            if (pts.length > 0) pieces.push(pts);
+            pts = [];
+            pts.push(pt);
+            lastPt = pt;
+        } else {
+            pts.push(pt);
+            lastPt = pt;
+        }
+        lastT = t1;
+    }
+    if (pts.length > 0) pieces.push(pts);
+
+    this._getScale(pieces);
+    var line = d3.svg.line().x(wrapIndex(this.x, 0)).y(wrapIndex(this.y, 1));
+
+    // Stick all the pieces together
+    var lineData = "";
+    for (var i = 0; i < pieces.length; ++i) {
+        if (pieces[i].length > 1) {
+            lineData += line(pieces[i]);
+        }
+    }
+    return this.svg.append("path").attr("d", lineData);
+  };
+
+  this.Plot.prototype._getScale = function (pts) {
+    if (this.x && this.y) return;
+
+    if (this.xbounds) {
+      this.x = d3.scale.linear()
+          .domain([this.xbounds[0], this.xbounds[1]])
+          .range([this.margin, this.width - this.margin]);
+    } else {
+      var flat = pts.reduce(function(a, b) {
+        return a.concat(b);
+      }).map(function(a) {
+          return a[0];
+      }).filter(function(a) {
+        return a;
+      });
+      this.x = d3.scale.linear()
+          .domain([d3.min(flat), d3.max(flat)])
+          .range([this.margin, this.width - this.margin]);
+    }
+
+    if (this.ybounds) {
+      this.y = d3.scale.linear()
+          .domain([this.ybounds[0], this.ybounds[1]])
+          .range([this.height - this.margin, this.margin]);
+    } else {
+      var flat = pts.reduce(function(a, b) {
+        return a.concat(b);
+      }).map(function(a) {
+          return a[1];
+      }).filter(function(a) {
+        return a;
+      });
+      this.y = d3.scale.linear()
+          .domain([d3.min(flat), d3.max(flat)])
+          .range([this.height - this.margin, this.margin]);
+    }
+  };
+
+  // ================================================================
+  // Private functions
+  function wrapIndex(f, i) {
+    return function (p) {
+      return f(p[i]);
+    };
+  }
+
+  function getIndex(i) {
+    return (function (x) {
+      return x[i];
+    });
+  }
+
+  function identity(x) {
+    return x;
+  }
+
+  function sign(a, b) {
+    if (a > b) return 1;
+    if (a < b) return -1;
+    return 0;
+  }
+
+  function checkAsymptote(fx, fy, t1, p1, t2, p2, n) {
+    if (t1 == null || p1 == null || n > 10) {
+      return false;
+    }
+
+    var mt = t1 + (t2 - t1) / 2;
+    var mx = fx(mt);
+    var my = fy(mt);
+    var mp = [mx, my];
+
+    if (Number.isNaN(mx) || !Number.isFinite(mx) ||
+        Number.isNaN(my) || !Number.isFinite(my)) {
+      return true;
+    }
+
+    // We have subdivided enough without finding an Inf or NaN.
+
+    var s1 = sign(p1[1], my);
+    var s2 = sign(my, p2[1]);
+    // Neither s1 or s2 are '0', i.e this section was not flat,
+    // and they are different, so derivative changed sign.
+    if (s1 && s2 && s1 != s2) {
+      if (n == 10) {
+        return true;
+      }
+      return checkAsymptote(fx, fy, t1, p1, mt, mp, n + 1) ||
+             checkAsymptote(fx, fy, mt, mp, t2, p2, n + 1);
+    }
+    return false;
+  };
+
+}).call(plottyjs);
